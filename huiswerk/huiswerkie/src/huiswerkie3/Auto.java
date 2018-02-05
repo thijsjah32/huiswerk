afb6e71375ad7620e45ff769c632f3c6dec14555
ThijsJanse <thijsjohanjan@gmail.com>
2018-02-06 00:08:00 +0100

    homocheck toegevoegd
    homocheck toegevoegd

---
+++ huiswerk/huiswerkie/src/huiswerkie3/Auto.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huiswerkie3;
+
+/**
+ *
+ * @author Stink Rat
+ */
+public class Auto {
+    private final boolean isGay = true;
+    private static String name;
+    
+    public static void main(String args[]) {
+        name = "Tinus";
+        
+        System.out.println(isGay("Tinus"));
+    }
+    
+    static String isGay(String name) {
+        if("Tinus".equals(name)) {
+            return "Ja die is best wel gay";
+        }
+        
+        return "Geintje natuurtlijk beetje gay is okay";
+    }
+}
+//hallo?//Hallo?????????????????????? druk of6
