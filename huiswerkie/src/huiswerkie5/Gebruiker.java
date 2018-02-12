62999958fbc7c1390189d06a36cfbe6da515056e
ThijsJanse <thijsjohanjan@gmail.com>
2018-02-12 22:49:51 +0100

    huiswerk 5
    in progress

---
+++ huiswerkie/src/huiswerkie5/Gebruiker.java
@@ -0,0 +1,31 @@
+
+package huiswerkie5;
+
+public class Gebruiker {
+    public String naam;
+    public int leeftijd;
+    
+    public Gebruiker(String naam, int leeftijd){
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+}
+    
+    public String toString(){
+        return naam + "(" + leeftijd + ")";
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+    public void setLeeftijd(int leeftijd) {
+        this.leeftijd = leeftijd;
+    }
+    
+    
+}
