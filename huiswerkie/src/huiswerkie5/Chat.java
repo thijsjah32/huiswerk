80290b43938942d40ff7e5548ed6b03feb74c0f9
ThijsJanse <thijsjohanjan@gmail.com>
2018-02-12 23:09:04 +0100

    nieuwe versie huiswerk 5

--- huiswerkie/src/huiswerkie5/Chat.java
+++ huiswerkie/src/huiswerkie5/Chat.java
@@ -4,30 +4,10 @@ package huiswerkie5;
 import java.util.ArrayList;
 
 public class Chat {
-    public String naam;
-    public static ArrayList<Bericht> berichten;
+    private String naam;
+    private ArrayList berichten;
     
     public Chat(String naam){
         this.naam = naam;
-        
-        Gebruiker gebruiker1 = new Gebruiker("jan", 47);
-        Gebruiker gebruiker2 = new Gebruiker("peter", 18);
-
-        Bericht bericht1 = new Bericht(gebruiker1, "wwar is mijn geld, godverdomme");
-        Bericht bericht2 = new Bericht(gebruiker2, "hey wolla, rustig G. ik betaal zekers wel, direct na scorro. aight?");
-        Bericht bericht3 = new Bericht(gebruiker1, "ok, maar waag het niet mij te dissen vieze buitenlander. anders vallen er klapjes");
-        
-        berichten = new ArrayList<>();
-        
-        berichten.add(bericht1);
-        berichten.add(bericht2);
-        berichten.add(bericht3);
     }
-    
-    private void voegToe(Bericht bericht){
-        
-    }
-    
-    
-    
 }
--- huiswerkie/src/huiswerkie5/main.java
+++ huiswerkie/src/huiswerkie5/main.java
@@ -1,21 +1,29 @@
 package huiswerkie5;
-import java.util.ArrayList;
 
 public class main {
-    private static ArrayList chats;
     
     public static void main(String[] args){
+        
+        Gebruiker gebruiker1 = new Gebruiker("jan", 47);
+        Gebruiker gebruiker2 = new Gebruiker("peter", 18);
 
-//        String aantal ="Er zijn: " + Chat.berichten.size() + " berichten:";
-//        System.out.println(aantal);
-        System.out.println(Chat.berichten.get(1));
+        Bericht bericht1 = new Bericht(gebruiker1, "wwar is mijn geld, godverdomme");
+        Bericht bericht2 = new Bericht(gebruiker2, "hey wolla, rustig G. ik betaal zekers wel, direct na scorro. aight?");
+        Bericht bericht3 = new Bericht(gebruiker1, "ok, maar waag het niet mij te dissen vieze buitenlander. anders vallen er klapjes");
+        
+        Bericht[] berichten = {
+            bericht1, bericht2, bericht3
+        };
 
-//        int nummer = 0;
-//
-//        for(int i=0; i<3; i++){
-//            System.out.println(Chat.berichten.get(nummer));
-//            nummer += 1;
-//        }
+        String aantal ="Er zijn: " + berichten.length + " berichten:";
+        System.out.println(aantal);
+
+        int nummer = 0;
+
+        for(int i=0; i<3; i++){
+            System.out.println(berichten[nummer]);
+            nummer += 1;
+        }
 
 
 
